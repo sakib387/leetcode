916b15d9ce327b52a9f76c3e69784f06bd4cde0e
sakib mollah <106031804+sakib387@users.noreply.github.com>
2023-10-20 23:39:22 +0600

    Time: 763 ms (11.76%), Space: 51.8 MB (5.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,21 +1,24 @@
 class Solution {
+    vector<int>v;
+    int dp[10008];
+    int jmp(int a){
+        if(a==v.size()-1)return 1;
+        if(v[a]==0)return 0;
+        if(dp[a]!=-1)return dp[a];
+        int c=a+v[a];
+        for(int i=a+1;i<=c;i++){
+            if(i<v.size()&&jmp(i)){
+                return dp[a]=1;
+            }
+        }
+        return dp[a]= 0;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return create(nums, 0, dp);
-    }
-private:
-    bool create(vector<int>& nums, int idx, vector<int>& dp) {
-        if(idx == nums.size() -1) return true;
-        if(nums[idx] == 0) return false;
+        v=nums;
+        for(int i=0;i<10005;i++)dp[i]=-1;
+        if( jmp(0))return true;
+        else return false;
         
-        if(dp[idx] != -1) return dp[idx]; //overlapping subproblems
-        int reach = idx + nums[idx];
-        for(int jump=idx + 1; jump <= reach; jump++) {
-            if(jump < nums.size() && create(nums, jump, dp)) 
-                return dp[idx] = true; //memoizing for particular index.
-        }
-        
-        return dp[idx] = false; //memoizing for particular index.
     }
 };
