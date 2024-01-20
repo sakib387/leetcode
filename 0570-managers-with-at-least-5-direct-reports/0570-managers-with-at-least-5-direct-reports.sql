ab9be2f775617c0ab8f01aa763cbcdd6da82b2eb
sakib mollah <106031804+sakib387@users.noreply.github.com>
2024-01-20 17:18:40 +0600

    Time: 3422 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT name 
 FROM Employee  e1
-where (select (count(managerId)) from Employee e2 where e2.managerId=e1.id)>=5
+where (select (count(id)) from Employee e2 where e2.managerId=e1.id)>=5
