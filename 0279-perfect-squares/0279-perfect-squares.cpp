d695da4c144b75942aaa602871097c62e1d189ac
sakib mollah <106031804+sakib387@users.noreply.github.com>
2024-04-21 22:52:12 +0600

    Time: 71 ms (45.13%), Space: 7.9 MB (89.58%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,23 +1,22 @@
 class Solution {
-    vector<int>v;
-    int dp[40000];
+    int dp[10005];
     int cal(int a){
-        if (a==0)return 0;
-        if(a<0)return INT_MAX;
+        if(a==0)return 0;
+        if(a==1)return 1;
         if(dp[a]!=-1)return dp[a];
-        int x=INT_MAX;
-        for(int i=0;i<v.size();i++){
-            if(v[i]<=a)x=min(x,1+cal(a-v[i]));
+        int c=INT_MAX;
+        for(int i=1;i*i<=a;i++){
+            c=min(c,1+cal(a-i*i));
         }
-        return dp[a]=x;
+        return dp[a]=c;
+        
+        
     }
 public:
     int numSquares(int n) {
+        int a=sqrt(n);
         memset(dp,-1,sizeof(dp));
-        for(int i=1;i*i<=n;i++){
-            v.push_back(i*i);
-        }
-        int l=cal(n);
-        return l;
+        return cal(n);
+        
     }
 };
