2f91d107d5d34edfe5db294a7cea8a979ff6bc8a
sakib mollah <106031804+sakib387@users.noreply.github.com>
2024-04-24 01:38:26 +0600

    Time: 170 ms (5.17%), Space: 53.3 MB (21.26%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& v) {
+       for(int i=0;i<v.size();){
+           int c=v[i];
+           if(i+1==c||v[i]<=0||v[i]>=v.size()||v[v[i]-1]==v[i]){i++;
+               continue;
+           }
+           else{
+                cout<<i<<" "<<v[i]<<" "<<v[v[i]-1]<<endl;
+               swap(v[i],v[v[i]-1]);
+               
+           }
+       }
+        cout<<endl;
         for(int i=0;i<v.size();i++){
-            if(v[i]<0)v[i]=0;
+         if(v[i]!=i+1)return i+1;
         }
-        for(int i=0;i<v.size();i++){
-            int c=abs(v[i])-1;
-            if(c>=0&&c<v.size()){
-                if(v[c]==0)v[c]=-(v.size()+10);
-                else if(v[c]>0)v[c]=-v[c];
-            }
-           // if(v[i]==0)v[i]=-(v.size()+10);
-            
-        }
-        int an=v.size()+1;
-        for(int i=1;i<=v.size();i++){
-             if(v[i-1]>=0){
-                 an=min(an,i);
-             }
-        }
-        return an;
+        return v.size()+1;
     }
 };
