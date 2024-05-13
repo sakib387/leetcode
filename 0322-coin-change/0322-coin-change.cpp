446f41b32773b3fefec09214870551ec004af50a
sakib mollah <106031804+sakib387@users.noreply.github.com>
2024-05-13 22:59:52 +0600

    Time: 36 ms (46.91%), Space: 13 MB (87.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    int dp[10004];
     vector<int>v;
-    int p=0;
-    int cal(int am){
-        
-        
-        if(am==0){
-            p=1;
-            return 0; 
+    int dp[100000];
+    int cal(int n){
+        if(n==0)return 0;
+        if(n<0)return 200000000;
+        if(dp[n]!=-1)return dp[n];
+        int an=200000000;
+        for(int i=0;i<v.size();i++){
+            an=min(an,cal(n-v[i])+1);
         }
-        if(am<0)return INT_MAX;
-        if(dp[am]!=-1)return dp[am];
-        int x=INT_MAX,an;
-        for(int i=v.size()-1;i>=0;i--){
-            if(am-v[i]>=0){
-                 an=cal(am-v[i]);
-            if(an!=INT_MAX)x=min(x,an+1);
-            }
-              
-        }
-        return dp[am]=x;
+        return dp[n]= an;
     }
     int coinChange(vector<int>& coins, int amount) {
-       memset(dp,-1,sizeof(dp));
-        v=coins;
-       
-        int a=cal(amount);
-        
-        if(p)return a;
-        else return -1;
+         v=coins;
+        memset(dp,-1,sizeof(dp));
+        int a= cal(amount);
+        if(a==200000000)a=-1;
+        return a;
     }
-    
 };
